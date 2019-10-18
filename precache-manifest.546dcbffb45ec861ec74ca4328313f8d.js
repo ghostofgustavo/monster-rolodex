4ceff5a9c291fd37fec62f3f274f4c70a3190962
mubashir <mubashir_meraj@ymail.com>
2019-10-19 00:44:56 +0500

    Updates

---
+++ precache-manifest.546dcbffb45ec861ec74ca4328313f8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10af56e32db8f0aa9fe53d646627bbcd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a3e0fd764f118d47babf",
+    "url": "/monster-rolodex/static/css/main.5fa6e06b.chunk.css"
+  },
+  {
+    "revision": "5fe7237698c5e0f93d8e",
+    "url": "/monster-rolodex/static/js/2.7ed93505.chunk.js"
+  },
+  {
+    "revision": "a3e0fd764f118d47babf",
+    "url": "/monster-rolodex/static/js/main.cd5ab934.chunk.js"
+  },
+  {
+    "revision": "e4a84cd1159e6b489641",
+    "url": "/monster-rolodex/static/js/runtime-main.74ebc4ac.js"
+  }
+]);
